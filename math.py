fd14a421ae46fcb54b73e10144f67e48dd7cf785
SushrutVaidya <91380515+SushrutVaidya@users.noreply.github.com>
2022-02-14 01:09:38 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,18 @@
+# demo
+def add(x,y):
+    return x+y
+# demo 
+def substract(x,y):
+    return x-y   '''on master branch'''
+# demo
+def multiply(x,y):
+    return x*y  
+# demo
+def divide(x,y):
+    if y == 0:
+        return "divide by 0 is erroe\r"
+    else :
+        x//y
+def square(x):
+
+    pass
